b8bb0067da96fa5f2f67d7a9e08c69d4da5f199a
mnwhryk <minomushi.ff15.07.08@gmail.com>
2020-11-17 06:56:24 +0000

    2 commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1871b886e80e2d8da4a71899c0b3543cfd8e675d260a42d723744fc3d22d0ef7cfae03adf9c4171af050a4102171e1438ac421b9d37a5509d16048ef79f3e1cd'
